56af6308a3f755a3edaf2a2152eb6b650ba3714e
CarlosLacerda92 <carloslacerda.092@gmail.com>
2023-10-29 21:58:44 -0300

    WIP: estruturação da página inicial

--- resources/views/components/cabecalho.blade.php
+++ resources/views/components/cabecalho.blade.php
@@ -1,4 +1,40 @@
-<div style="display: flex; align-items: center; justify-content: space-between;">
-    <i class="fa-solid fa-magnifying-glass"></i>
-    <i class="fa-solid fa-bell"></i>
+<div class="cabecalho">
+
+    <div class="logo">
+        QuickTaste
+    </div>
+
+   {{--  <input type="text" class="qk-input" placeholder="Busque restaurantes aqui!" /> --}}
+
+   <div class="cabecalho-menus">
+
+        <span>
+            Home
+        </span>
+
+        <span>
+            Restaurantes
+        </span>
+        
+        <span>
+            Ajuda
+        </span>
+   </div>
+
+    <div style="display: flex; align-items: center; gap: 1rem;">
+
+        <div style="display: flex; aligin-items: center; gap: 1.5rem;">
+            <i class="fa-solid fa-magnifying-glass" style="font-size: 1.25rem;"></i>
+            <i class="fa-solid fa-bell" style="font-size: 1.25rem;"></i>
+            <i class="fa-solid fa-cart-shopping" style="font-size: 1.25rem;"></i>
+        </div>
+        
+        <div class="cabecalho-perfil">
+            <img class="foto-perfil" src="{{ asset('storage/fotos_perfil/id_3.png') }}" />
+            <span>
+                Nome
+            </span>
+            <i class="fa-solid fa-chevron-down"></i>
+        </div>
+    </div>
 </div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,19 +1,21 @@
 
 <x-inicio />
 
-<div style="width: 5%;">
-    <x-barra-lateral />
-</div>
+<x-cabecalho />
 
-<div style="width: 95%;">
-    <x-cabecalho />
-    <div>
-        <h1>
-            Bem-vindo, Fulano!
-        </h1>
-        <h3>
-            Mensagem legal aqui.
-        </h3>
+<div class="princ">
+    <div style="display: flex; align-items: center; gap: 1rem;">
+
+        <img src="{{ asset('storage/fotos_perfil/id_3.png') }}" style="width: 5rem;" />
+
+        <div style="display: flex; flex-direction: column;">
+            <h1 style="margin: 0;">
+                Bem-vindo, Fulano!
+            </h1>
+            <h3 style="margin: 0;">
+                Mensagem legal aqui.
+            </h3>
+        </div>
     </div>
 </div>
 
