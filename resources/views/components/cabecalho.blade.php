cf05be2a1aaf20970c41b29f8b127dce10aeac1b
CarlosLacerda92 <carloslacerda.092@gmail.com>
2023-10-30 21:35:20 -0300

    WIP: tela de restaurantes

--- resources/views/components/cabecalho.blade.php
+++ resources/views/components/cabecalho.blade.php
@@ -4,36 +4,29 @@
         QuickTaste
     </div>
 
-   {{--  <input type="text" class="qk-input" placeholder="Busque restaurantes aqui!" /> --}}
-
    <div class="cabecalho-menus">
-
         <span>
             Home
         </span>
 
         <span>
-            Restaurantes
+            Sobre
         </span>
         
         <span>
-            Ajuda
+            Contato
         </span>
    </div>
 
-    <div style="display: flex; align-items: center; gap: 1rem;">
+    <div style="display: flex; align-items: center; gap: 2rem;">
 
-        <div style="display: flex; aligin-items: center; gap: 1.5rem;">
-            <i class="fa-solid fa-magnifying-glass" style="font-size: 1.25rem;"></i>
+        <div style="display: flex; aligin-items: center; gap: 2rem;">
             <i class="fa-solid fa-bell" style="font-size: 1.25rem;"></i>
             <i class="fa-solid fa-cart-shopping" style="font-size: 1.25rem;"></i>
         </div>
         
         <div class="cabecalho-perfil">
             <img class="foto-perfil" src="{{ asset('storage/fotos_perfil/id_3.png') }}" />
-            <span>
-                Nome
-            </span>
             <i class="fa-solid fa-chevron-down"></i>
         </div>
     </div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,18 +4,23 @@
 <x-cabecalho />
 
 <div class="princ">
-    <div style="display: flex; align-items: center; gap: 1rem;">
+    
+    <div class="div-bem-vindo">
+        
+        {{-- <div style="display: flex; align-items: center; gap: 1rem;"> --}}
 
-        <img src="{{ asset('storage/fotos_perfil/id_3.png') }}" style="width: 5rem;" />
+            {{-- <img src="{{ asset('storage/fotos_perfil/id_3.png') }}" style="width: 5rem;" /> --}}
 
-        <div style="display: flex; flex-direction: column;">
-            <h1 style="margin: 0;">
-                Bem-vindo, Fulano!
-            </h1>
-            <h3 style="margin: 0;">
-                Mensagem legal aqui.
-            </h3>
-        </div>
+            <div style="display: flex; flex-direction: column; justify-content: center; width: 100%;">
+                <h1 style="margin: 0;">
+                    Bem-vindo, Fulano!
+                </h1>
+                <h3 style="margin: 0;">
+                    Como está sua fome hoje?
+                </h3>
+            </div>
+        {{-- </div> --}}
+        <input type="text" class="qk-input" placeholder="Descubra pratos, restaurantes e mais!" style=" width: 50%" /> 
     </div>
 </div>
 
